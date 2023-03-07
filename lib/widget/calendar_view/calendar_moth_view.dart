7e84567678a1609772fc3c311a75d0487fa7f1aa
Tran Hieu <82859719+tranhieudevvietnam@users.noreply.github.com>
2023-03-07 15:34:15 +0700

    update calendar

--- lib/widget/calendar_view/calendar_moth_view.dart
+++ lib/widget/calendar_view/calendar_moth_view.dart
@@ -19,7 +19,8 @@ class CalendarMothView extends StatelessWidget {
 
   int? dayUpdate;
   int dayForFirstRow = 0;
-  DateTime myDateAfterPlus = DateTime.now().subtract(const Duration(days: 31));
+  DateTime myDateAfterPlus =
+      DateTime.now().subtract(Duration(days: DateTime.now().daysInMonth()));
   final maxRowsNumber = 5;
   @override
   Widget build(BuildContext context) {
@@ -49,12 +50,12 @@ class CalendarMothView extends StatelessWidget {
       if (dayUpdate != null) {
         date = DateTime(dateTime.year, dateTime.month, dayUpdate!);
       }
-      if (myDateAfterPlus.month <= date.month && myDateAfterPlus.day != 1) {
+      // if (myDateAfterPlus.month <= date.month && myDateAfterPlus.day != 1) {
         rows.add(_buildRow(
           dateTimeDefault: date,
           context: context,
         ));
-      }
+      // }
     }
 
     return rows;
@@ -112,5 +113,45 @@ class CalendarMothView extends StatelessWidget {
       mainAxisSize: MainAxisSize.min,
       children: widget,
     );
+  }
+}
+
+extension CustomDateTime on DateTime {
+  int daysInMonth() {
+    if (month > 12) {
+      return 1;
+    }
+    final List<int> monthLength = List.filled(12, 0);
+    monthLength[0] = 31;
+    monthLength[2] = 31;
+    monthLength[4] = 31;
+    monthLength[6] = 31;
+    monthLength[7] = 31;
+    monthLength[9] = 31;
+    monthLength[11] = 31;
+    monthLength[3] = 30;
+    monthLength[8] = 30;
+    monthLength[5] = 30;
+    monthLength[10] = 30;
+
+    if (leapYear(year) == true) {
+      monthLength[1] = 29;
+    } else {
+      monthLength[1] = 28;
+    }
+
+    return monthLength[month - 1];
+  }
+
+  bool leapYear(int year) {
+    bool leapYear = false;
+    final bool leap = (year % 100 == 0) && (year % 400 != 0);
+    if (leap == true) {
+      leapYear = false;
+    } else if (year % 4 == 0) {
+      leapYear = true;
+    }
+
+    return leapYear;
   }
 }
